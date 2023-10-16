58b57407520eca0af0e59fd29a61aa88be4b7d92
rahmetXD <144803190+rahmetXD@users.noreply.github.com>
2023-10-16 23:49:51 +0300

    Update mentionall.py

--- mentionall.py
+++ mentionall.py
@@ -170,7 +170,7 @@ async def mentionall(event):
       usrnum += 1
       usrtxt += f"[{random.choice(bayrak)}](tg://user?id={usr.id}) "
       if event.chat_id not in anlik_calisan:
-   return await event.respond(responses["sebeo"])
+   return await event.respond(responses["basarı"])
       if usrnum == 5:
         await client.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
         await asyncio.sleep(2)
